fef2ec4b69a4ed921875ae0e23f6390d754227e9
3llousha <164997171+3llousha@users.noreply.github.com>
2024-06-24 17:08:44 +0200

    Update ALU_tb.vhd

--- ALU_tb.vhd
+++ ALU_tb.vhd
@@ -3,12 +3,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Entity declaration for the testbench
 entity tb_ALU is
 end tb_ALU;
 
--- Architecture definition for the testbench
-architecture Behavioral of tb_ALU is
+architecture Behavioral of tb_ALU is -- Architecture definition for the testbench
     component ALU
         Port ( A,B : in  STD_LOGIC_VECTOR (7 downto 0);
                Cin : in  STD_LOGIC;
@@ -16,17 +14,15 @@ architecture Behavioral of tb_ALU is
                Y : out  STD_LOGIC_VECTOR (7 downto 0);
                Cout : out  STD_LOGIC);
     end component;
-
-    -- Signals to connect to ALU
-    signal A, B : STD_LOGIC_VECTOR(7 downto 0);
+    
+    signal A, B : STD_LOGIC_VECTOR(7 downto 0);     -- Signals to connect to ALU
     signal Cin : STD_LOGIC;
     signal Sel : STD_LOGIC_VECTOR(1 downto 0);
     signal Y : STD_LOGIC_VECTOR(7 downto 0);
     signal Cout : STD_LOGIC;
-
 begin
-    -- Unit Under Test
-    uut: ALU Port map (
+
+    uut: ALU Port map ( -- Unit Under Test
         A => A,
         B => B,
         Cin => Cin,
@@ -34,26 +30,10 @@ begin
         Y => Y,
         Cout => Cout
     );
-
-
     process
     begin
-        -- Test case 1: A + B = 2,  A = 1, B = 1, Cin = 0, Sel = 00
+        -- Test case  A + B = 2,  A = 1, B = 1, Cin = 0, Sel = 00
         A <= "00000001"; B <= "00000001"; Cin <= '0'; Sel <= "00";
-        wait for 10 ns;
-
-        -- Test case 2: A and B = 1, A = 1, B = 1, Sel = 01
-        Sel <= "01";
-        wait for 10 ns;
-
-        -- Test case 3: A or B = 1 -- A = 1, B = 1, Sel = 10
-        Sel <= "10";
-        wait for 10 ns;
-
-        -- Test case 4: Invert A (~1 = 254): A = 1 , Sel = 11
-        Sel <= "11";
-        wait for 10 ns;
-
-        wait;
+wait;
     end process;
 end Behavioral;
