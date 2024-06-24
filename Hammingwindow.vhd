55be63eff0eda467609da8e4bd1d3db5c6c1a152
3llousha <164997171+3llousha@users.noreply.github.com>
2024-06-25 00:14:14 +0200

    Update Hammingwindow.vhd

--- Hammingwindow.vhd
+++ Hammingwindow.vhd
@@ -19,14 +19,16 @@ begin
         AVG_OUT <= (others => '0');
     elsif rising_edge(CLK) then
         if ENABLE = '1' then
-            sum_with_window <= std_logic_vector(to_unsigned(to_integer(unsigned(PWM_IN)) * Hamming_Coefficients(to_integer(unsigned(count))), 16));
+            sum_with_window <= std_logic_vector(to_unsigned(to_integer(unsigned(PWM_IN)) 
+        * Hamming_Coefficients(to_integer(unsigned(count))), 16));
             sum_reg <= sum_reg + sum_with_window;
             count <= count + 1;
         end if;
 
         if DATA_READY = '1' then
             if count /= 0 then
-                AVG_OUT <= std_logic_vector(to_unsigned(to_integer(unsigned(sum_reg)) / to_integer(unsigned(count)), 8));
+                AVG_OUT <= std_logic_vector(to_unsigned(to_integer(unsigned(sum_reg)) 
+            / to_integer(unsigned(count)), 8));
             end if;
             sum_reg <= (others => '0');
             count <= (others => '0');
