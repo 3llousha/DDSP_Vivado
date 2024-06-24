5da04903241a66aeeff3fcfaa135b8c08279e6b8
3llousha <164997171+3llousha@users.noreply.github.com>
2024-06-24 20:54:42 +0200

    Update DataPath.vhd

--- DataPath.vhd
+++ DataPath.vhd
@@ -1,63 +1,54 @@
--- Top-Level Data Path
+-- Datapath for Microcontroller
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity DataPath is
-    Port ( CLK      : in  STD_LOGIC;
-           RESET    : in  STD_LOGIC;
-           LOAD_A, LOAD_B,LOAD_C,LOAD_D : in  STD_LOGIC;
-           SEL_A, SEL_B    : in  STD_LOGIC_VECTOR (1 downto 0);
-           ALU_SEL  : in  STD_LOGIC_VECTOR (1 downto 0);
-           ALU_CIN  : in  STD_LOGIC;
-           INPUT_A,INPUT_B  : in  STD_LOGIC_VECTOR (7 downto 0);
-           OUTPUT_A,OUTPUT_B,OUTPUT_C,OUTPUT_D : out  STD_LOGIC_VECTOR (7 downto 0);
-           ALU_OUT  : out  STD_LOGIC_VECTOR (7 downto 0));
-end DataPath;
+entity Datapath is
+    Port ( CLK,RESET      : in  STD_LOGIC;
+           ALU_OP      : in  STD_LOGIC_VECTOR (1 downto 0);
+           LOAD_A,LOAD_B,LOAD_C,LOAD_D : in  STD_LOGIC;
+           SEL_A,SEL_B : in  STD_LOGIC_VECTOR (1 downto 0);
+           IN_PORT0,IN_PORT1 : in  STD_LOGIC_VECTOR (7 downto 0);
+           OUT_PORT0, OUT_PORT1 : out STD_LOGIC_VECTOR (7 downto 0);
+            
+end Datapath;
 
-architecture Structural of DataPath is
-    signal A, B, C, D : STD_LOGIC_VECTOR (7 downto 0);
-    signal MUX_A_OUT, MUX_B_OUT, ALU_RESULT : STD_LOGIC_VECTOR (7 downto 0);
-    signal ALU_COUT : STD_LOGIC;
-
-    component Register8
-        Port ( D     : in  STD_LOGIC_VECTOR (7 downto 0);
-               CLK   : in  STD_LOGIC;
-               LOAD  : in  STD_LOGIC;
-               Q     : out  STD_LOGIC_VECTOR (7 downto 0));
-    end component;
-
-    component Mux4x8
-        Port ( S : in  STD_LOGIC_VECTOR (1 downto 0);
-               D0,D1,D2,D3 : in  STD_LOGIC_VECTOR (7 downto 0);
-               Y : out  STD_LOGIC_VECTOR (7 downto 0));
-    end component;
-
-    component ALU
-        Port ( A,B : in  STD_LOGIC_VECTOR (7 downto 0);
-               Cin : in  STD_LOGIC;
-               Sel : in  STD_LOGIC_VECTOR (1 downto 0);
-               Y : out  STD_LOGIC_VECTOR (7 downto 0);
-               Cout : out  STD_LOGIC);
-    end component;
-
+architecture Structural of Datapath is
+    -- Signals for register outputs
+    signal Q_A, Q_B, Q_C, Q_D : STD_LOGIC_VECTOR (7 downto 0);
+    -- Signals for ALU inputs and output
+    signal ALU_A, ALU_B, ALU_Y : STD_LOGIC_VECTOR (7 downto 0);
+    signal ALU_Cout : STD_LOGIC;
 begin
-    -- Instantiate Registers
-    regA: Register8 Port map (D => INPUT_A, CLK => CLK, LOAD => LOAD_A, Q => A);
-    regB: Register8 Port map (D => INPUT_B, CLK => CLK, LOAD => LOAD_B, Q => B);
-    regC: Register8 Port map (D => ALU_RESULT, CLK => CLK, LOAD => LOAD_C, Q => C);
-    regD: Register8 Port map (D => ALU_RESULT, CLK => CLK, LOAD => LOAD_D, Q => D);
+    -- Instantiating Registers
+    Reg_A: entity work.Register8Bit Port map (D => ALU_Y, CLK => CLK, LOAD => LOAD_A, Q => Q_A);
+    Reg_B: entity work.Register8Bit Port map (D => ALU_Y, CLK => CLK, LOAD => LOAD_B, Q => Q_B);
+    Reg_C: entity work.Register8Bit Port map (D => ALU_Y, CLK => CLK, LOAD => LOAD_C, Q => Q_C);
+    Reg_D: entity work.Register8Bit Port map (D => ALU_Y, CLK => CLK, LOAD => LOAD_D, Q => Q_D);
 
-    -- Instantiate Multiplexers
-    muxA: Mux4x8 Port map (S => SEL_A, D0 => A, D1 => B, D2 => C, D3 => D, Y => MUX_A_OUT);
-    muxB: Mux4x8 Port map (S => SEL_B, D0 => A, D1 => B, D2 => C, D3 => D, Y => MUX_B_OUT);
+    -- Instantiating Multiplexers
+    MuxA: entity work.Mux4x8 Port map (S => SEL_A, D0 => Q_A, D1 => Q_B, D2 => Q_C, D3 => Q_D, Y => ALU_A);
+    MuxB: entity work.Mux4x8 Port map (S => SEL_B, D0 => Q_A, D1 => Q_B, D2 => Q_C, D3 => Q_D, Y => ALU_B);
 
-    -- Instantiate ALU
-    alu_inst: ALU Port map (A => MUX_A_OUT, B => MUX_B_OUT, Cin => ALU_CIN, Sel => ALU_SEL, Y => ALU_RESULT, Cout => ALU_COUT);
+    -- Instantiating ALU
+    ALU_Inst: entity work.ALU Port map (A => ALU_A, B => ALU_B, Cin => '0', Sel => ALU_OP, Y => ALU_Y, Cout => ALU_Cout);
 
-    -- Output assignments
-    OUTPUT_A <= A;
-    OUTPUT_B <= B;
-    OUTPUT_C <= C;
-    OUTPUT_D <= D;
-    ALU_OUT  <= ALU_RESULT;
+    -- Input Ports to Registers
+    process (CLK)
+    begin
+        if rising_edge(CLK) then
+            if RESET = '1' then
+                Q_A <= (others => '0');
+                Q_B <= (others => '0');
+                Q_C <= (others => '0');
+                Q_D <= (others => '0');
+            end if;
+        end if;
+    end process;
+
+    -- Output Ports from Registers
+    OUT_PORT0 <= Q_A;
+    OUT_PORT1 <= Q_B;
+
 end Structural;
